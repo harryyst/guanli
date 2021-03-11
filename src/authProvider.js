f3271f775df9cb0ae8311be6984ff28388e684a5
Frank <873634000@qq.com>
2021-03-11 21:23:54 +1000

    admin login/out

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,9 +1,25 @@
 export default {
     // called when the user attempts to log in
-    login: ({ username }) => {
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+    login: ({ username, password }) =>  {
+        const request = new Request('http://localhost:4000/api/students/authenticate', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error('username of password wrong');
+                }
+                console.log(response)
+                return response.json();
+            })
+            .then(auth => {
+                localStorage.setItem('username', JSON.stringify(auth));
+            })
+            .catch(() => {
+                throw new Error('username of password wrong')
+            });
     },
     // called when the user clicks on the logout button
     logout: () => {
